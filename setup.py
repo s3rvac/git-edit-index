b3b5caa7ea42485f6836cb376ce67cd2188045cf
Petr Zemek <s3rvac@gmail.com>
2017-08-07 21:40:09 +0200

    Add Python 3.6 into 'classifiers' in setup.py.

--- setup.py
+++ setup.py
@@ -75,6 +75,7 @@ more information.
         'Programming Language :: Python :: 3.3',
         'Programming Language :: Python :: 3.4',
         'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: Implementation :: CPython',
         'Programming Language :: Python :: Implementation :: PyPy',
         'Topic :: Software Development :: Version Control',
